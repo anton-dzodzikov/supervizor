e7e0a74c72b7cd0071cd321bdeea055f9cab5961
a.dzodzikov <a.dzodzikov@cy.cardpay.com>
2018-10-29 19:27:15 +1000

    Add exception mapping for Jersey

---
+++ src/main/java/supervizor/api/ExceptionMapper.java
@@ -0,0 +1,16 @@
+package supervizor.api;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Throwable> {
+    @Override
+    public Response toResponse(Throwable exception) {
+        return Response.serverError()
+                .entity(exception.getMessage())
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
